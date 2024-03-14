b4d544599c026f7da9d457a9b831ab11c0e8d6eb
TarasSabat <tarsab25@gmail.com>
2024-03-14 20:56:06 +0200

    1

--- Python WEB 1.1/Modul_6_Python-WEB/DZ/starter_query.py
+++ Python WEB 1.1/Modul_6_Python-WEB/DZ/starter_query.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 request_number = int(input("Enter the request number >>> "))
 if request_number == 1:
     a = "query_1.sql"
@@ -8,27 +9,40 @@ elif request_number == 2:
 elif request_number == 3:
     a = "query_3.sql"
     # Введення значення subject_id
-    subject_id = input("Введіть id предмета >>> ")
+    id = input("Введіть id предмета >>> ")
 elif request_number == 4:
     a = "query_4.sql"
 elif request_number == 5:
     a = "query_5.sql"
     # Введення значення teacher_id
-    teacher_id = input("Введіть id викладача >>> ")
+    id = input("Введіть id викладача >>> ")
 elif request_number == 6:
     a = "query_6.sql"
     # Введення значення group_id
-    group_id = input("Введіть id групи >>> ")
+    id = input("Введіть id групи >>> ")
 elif request_number == 7:
     a = "query_7.sql"
+    # Введення значення group_id
+    id_1 = input("Введіть id групи >>> ")
+    # Введення значення subject_id
+    id_2 = input("Введіть id предмета >>> ")
 elif request_number == 8:
     a = "query_8.sql"
+    # Введення значення teacher_id
+    id = input("Введіть id викладача >>> ")
 elif request_number == 9:
     a = "query_9.sql"
+    # Введення значення fullname
+    id = input("Введіть fullname студента >>> ")
 elif request_number == 10:
     a = "query_10.sql"
+    # Введення значення students_fullname
+    id_1 = input("Введіть fullname студента >>> ")
+    # Введення значення teachers_fullname
+    id_2 = input("Введіть fullname викладача >>> ")
 else:
-    print("Invalid request number")
+    print("Недійсний номер запиту")
+
 # Підключення до бази даних SQLite
 conn = sqlite3.connect("DZ_Mod_6")
 
@@ -40,43 +54,34 @@ with open(a, "r") as file:
     sql = file.read()
 
 
-# Виконання SQL-запиту для subject_id (query_3.sql)
-def execute_query_with_subject_id(subject_id=None):
-    if subject_id is not None:
-        cursor.execute(sql, (subject_id,))
+# Виконання SQL-запиту для query_3.sql, query_5.sql, query_6.sql, query_8.sql, query_9.sql
+
+
+def execute_query_3_5_6_8_9(id=None):
+    if id is not None:
+        cursor.execute(sql, (id,))
     else:
         cursor.execute(sql)
 
 
-# Виклик функції з аргументом subject_id, якщо він був визначений
-if "subject_id" in locals() and request_number == 3:
-    execute_query_with_subject_id(subject_id)
+# Виклик функції з аргументом, якщо він був визначений
+if "id" in locals() and request_number in (3, 5, 6, 8, 9):
+    execute_query_3_5_6_8_9(id)
+elif request_number in (1, 2, 4):
+    execute_query_3_5_6_8_9()
 
 
-# Виконання SQL-запиту для teacher_id (query_5.sql)
-def execute_query_with_teacher_id(teacher_id=None):
-    if teacher_id is not None:
-        cursor.execute(sql, (teacher_id,))
+# Виконання SQL-запиту для query_7.sql, query_10.sql
+def execute_query_7_10(id_1=None, id_2=None):
+    if id_1 and id_2 is not None:
+        cursor.execute(sql, (id_1, id_2))
     else:
         cursor.execute(sql)
 
 
-# Виклик функції з аргументом teacher_id, якщо він був визначений
-if "teacher_id" in locals() and request_number == 5:
-    execute_query_with_teacher_id(teacher_id)
-
-
-# Виконання SQL-запиту для group_id (query_6.sql)
-def execute_query_with_teacher_id(group_id=None):
-    if group_id is not None:
-        cursor.execute(sql, (group_id,))
-    else:
-        cursor.execute(sql)
-
-
-# Виклик функції з аргументом group_id, якщо він був визначений
-if "group_id" in locals() and request_number == 6:
-    execute_query_with_teacher_id(group_id)
+# Виклик функції з аргументом, якщо він був визначений
+if "id_1" and "id_2" in locals() and request_number in (7, 10):
+    execute_query_7_10(id_1, id_2)
 
 # Отримання результатів (якщо потрібно)
 results = cursor.fetchall()
--- Python WEB 1.1/Modul_6_Python-WEB/DZ/test.py
+++ Python WEB 1.1/Modul_6_Python-WEB/DZ/test.py
@@ -1,40 +1,75 @@
 import sqlite3
 
+request_number = int(input("Enter the request number >>> "))
+if request_number == 1:
+    a = "query_1.sql"
+elif request_number == 2:
+    a = "query_2.sql"
+elif request_number == 3:
+    a = "query_3.sql"
+    # Введення значення subject_id
+    id_1 = input("Введіть id предмета >>> ")
+elif request_number == 4:
+    a = "query_4.sql"
+elif request_number == 5:
+    a = "query_5.sql"
+    # Введення значення teacher_id
+    id_1 = input("Введіть id викладача >>> ")
+elif request_number == 6:
+    a = "query_6.sql"
+    # Введення значення group_id
+    id_1 = input("Введіть id групи >>> ")
+elif request_number == 7:
+    a = "query_7.sql"
+    # Введення значення group_id
+    id_1 = input("Введіть id групи >>> ")
+    # Введення значення subject_id
+    id_2 = input("Введіть id предмета >>> ")
+elif request_number == 8:
+    a = "query_8.sql"
+    # Введення значення teacher_id
+    id_1 = input("Введіть id викладача >>> ")
+elif request_number == 9:
+    a = "query_9.sql"
+    # Введення значення fullname
+    id_1 = input("Введіть fullname студента >>> ")
+elif request_number == 10:
+    a = "query_10.sql"
+else:
+    print("Invalid request number")
+# Підключення до бази даних SQLite
+conn = sqlite3.connect("DZ_Mod_6")
 
-class DatabaseQuery:
-    def __init__(self, database_name):
-        self.conn = sqlite3.connect(database_name)
-        self.cursor = self.conn.cursor()
+# Створення курсора
+cursor = conn.cursor()
 
-    def execute_query(self, query, parameters=None):
-        if parameters:
-            self.cursor.execute(query, parameters)
-        else:
-            self.cursor.execute(query)
-        results = self.cursor.fetchall()
-        for row in results:
-            print(row)
-
-    def close_connection(self):
-        self.cursor.close()
-        self.conn.close()
+# Зчитування вмісту файлу query_number.sql
+with open(a, "r") as file:
+    sql = file.read()
 
 
-def main():
-    request_number = int(input("Enter the request number >>> "))
-    query_file = f"query_{request_number}.sql"
-
-    db = DatabaseQuery("DZ_Mod_6.db")
-
-    if request_number in [3, 5]:
-        parameter = input("Enter the parameter value >>> ")
-
-    with open(query_file, "r") as file:
-        query = file.read()
-
-    db.execute_query(query, (parameter,) if request_number in [3, 5] else None)
-    db.close_connection()
+# Виконання SQL-запиту для query_3.sql, query_5.sql, query_6.sql, query_8.sql, query_9.sql
+def execute_query(id_1=None, id_2=None):
+    if id_1 is not None and id_2 is None:
+        cursor.execute(sql, (id_1, id_2))
+    if id_1 and id_2 is not None:
+        cursor.execute(sql, (id_1, id_2))
+    else:
+        cursor.execute(sql)
 
 
-if __name__ == "__main__":
-    main()
+# Виклик функції з аргументом, якщо він був визначений
+if "id_1" and "id_2" in locals() and request_number == 3:  # or 5 or 6 or 8 or 9:
+    execute_query(id_1, id_2)
+
+# Отримання результатів (якщо потрібно)
+results = cursor.fetchall()
+for row in results:
+    print(row)
+
+# Збереження змін у базі даних
+conn.commit()
+
+# Закриття курсора та з'єднання з базою даних
+cursor.close()
+conn.close()
