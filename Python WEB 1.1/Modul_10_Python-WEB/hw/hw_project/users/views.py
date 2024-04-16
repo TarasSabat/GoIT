6d5089d26385e06abe38e2120ba79c6f8e5d2ed6
TarasSabat <tarsab25@gmail.com>
2024-04-16 19:02:46 +0300

    1

--- Python WEB 1.1/Modul_10_Python-WEB/hw/hw_project/users/views.py
+++ Python WEB 1.1/Modul_10_Python-WEB/hw/hw_project/users/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+# views.py
+from flask import Flask, request, session, redirect, url_for, abort, render_template, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from pymongo import MongoClient
 
-# Create your views here.
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(
+    MONGO_URI='mongodb://localhost:27017/myDatabase',
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+
+def get_db():
+    client = MongoClient(app.config['MONGO_URI'])
+    return client.myDatabase
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.users.find_one({'username': username}) is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.users.insert_one({'username': username, 'password': generate_password_hash(password)})
+            return redirect(url_for('login'))
+
+        flash(error)
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.users.find_one({'username': username})
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = str(user['_id'])
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
